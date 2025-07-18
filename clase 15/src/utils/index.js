165256d612987393e968cf274ec36ba218a8a1c4
InstDanielRiverol <d.riverol@bue.edu.ar>
2025-07-17 21:31:35 -0300

    Add files via upload

---
+++ clase 15/src/utils/index.js
@@ -0,0 +1,9 @@
+// utils
+
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(dirname(__filename), "..");
+
+export { __dirname, join };
